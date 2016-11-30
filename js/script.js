a605918612843d46cdbefa0e7b3ad7706629f84b
Vince Campanale <vcampanale0294@gmail.com>
2016-11-30 17:36:16 -0500

    number buttons and decimal button working - current number displaying. also got negative button working, but it needs improvement

--- js/script.js
+++ js/script.js
@@ -16,3 +16,84 @@
   //If current-number contains a positive value, tack on a negative to the front of current-number
   //If current-number contains a negative value, remove the negative sign in front of current-number
   //If current-number is empty, do nothing
+
+//add event listeners to relevant numbers and symbols. wrapped in an IIFE to keep global scope clean
+(function numberEventListeners(){
+  var one = document.getElementById("1");
+  var two = document.getElementById("2");
+  var three = document.getElementById("3");
+  var four = document.getElementById("4");
+  var five = document.getElementById("5");
+  var six = document.getElementById("6");
+  var seven = document.getElementById("7");
+  var eight = document.getElementById("8");
+  var nine = document.getElementById("9");
+  var zero = document.getElementById("0");
+  var dot = document.getElementById("dot");
+  var negative = document.getElementById("negative");
+  one.addEventListener("click", function(){pushSymbol(1)});
+  two.addEventListener("click", function(){pushSymbol(2)});
+  three.addEventListener("click", function(){pushSymbol(3)});
+  four.addEventListener("click", function(){pushSymbol(4)});
+  five.addEventListener("click", function(){pushSymbol(5)});
+  six.addEventListener("click", function(){pushSymbol(6)});
+  seven.addEventListener("click", function(){pushSymbol(7)});
+  eight.addEventListener("click", function(){pushSymbol(8)});
+  nine.addEventListener("click", function(){pushSymbol(9)});
+  zero.addEventListener("click", function(){pushSymbol(0)});
+  dot.addEventListener("click", function(){pushSymbol('.')});
+  
+  //change this to handlNegative function - some edge cases not considered here
+  negative.addEventListener("click", function(){pushSymbol('-')});
+})();
+
+
+
+//push the pressed number onto the displayed number
+function pushSymbol(num){
+  var currentNumber = getCurrentNumber();
+  var numAsString = num.toString();
+  var currentNumberAsString = currentNumber.toString();
+  var array = currentNumberAsString.split('');
+
+  //TODO: separate this logic into separate function called handleNegative
+  if(num == '-' && array[0] == "-"){
+    array.splice(0, 1);
+    var newDisplay = array.join('');
+    var node = document.getElementById("currentNumber");
+    node.innerHTML = newDisplay;
+    return;
+  }
+
+  if(num == '-' && array[0] !== "-"){
+    array.unshift(num);
+    var newDisplay = array.join('');
+    var node = document.getElementById("currentNumber");
+    node.innerHTML = newDisplay;
+    return;
+  }
+
+  array.push(num);
+  var newDisplay = array.join('');
+
+  var node = document.getElementById("currentNumber");
+  node.innerHTML = newDisplay;
+}
+
+
+function getCurrentNumber(){
+  var node = document.getElementById("currentNumber");
+  var number = node.innerHTML.trim();
+  return number;
+}
+
+function add(){
+  var currentNumber = getCurrentNumber();
+}
+
+var filterFloat = function (value) {
+    if(/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
+      .test(value))
+      return Number(value);
+  return NaN;
+}
